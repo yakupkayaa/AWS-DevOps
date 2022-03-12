aa455adb5cfb55db07ca7d0646340bd43d455fa1
yakupkaya <yakupkayaa@gmail.com>
2022-03-12 10:24:23 +0300

    ok

--- aws/Hands-on/03_10_2022-Guile-VPC4/VPC4-LAMP-NACL.sh
+++ aws/Hands-on/03_10_2022-Guile-VPC4/VPC4-LAMP-NACL.sh
@@ -13,24 +13,7 @@
    Sec Group: "Wordpress-BastionHost-SG"
    User Data:
    
-#!/bin/bash
-
-yum update -y
-amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-wget https://wordpress.org/latest.tar.gz
-tar -xzf latest.tar.gz
-sudo cp -r wordpress/* /var/www/html/
-cd /var/www/html/
-cp wp-config-sample.php wp-config.php
-chown -R apache /var/www
-chgrp -R apache /var/www
-chmod 775 /var/www
-find /var/www -type d -exec sudo chmod 2775 {} \;
-find /var/www -type f -exec sudo chmod 0664 {} \;
-systemctl restart httpd
+c
 
 # 3.Create MariaDB ec2 instance in Private Subnet 1b
 
